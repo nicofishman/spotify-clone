a58cc54afe332228f9532d916ea3c346c6230b3d
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-03-14 09:40:39 -0300

    log in button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
-import NowPlaying from '@/components/Layout/NowPlaying';
-import Sidebar from '@/components/Layout/Sidebar';
-import TopBar from '@/components/Layout/TopBar';
+import { Sidebar, NowPlaying, TopBar } from '@/components/Layout';
+import { signIn, useSession } from 'next-auth/react';
 import React from 'react';
 
 interface LayoutProps {
@@ -8,12 +7,22 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
-	return (
+	const { data: session } = useSession();
+	return session ? (
 		<div className='grid h-screen w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] grid-areas-layout'>
 			<Sidebar />
 			<TopBar />
 			<main className='bg-blue-500 grid-in-main-view'>{children}</main>
 			<NowPlaying />
+		</div>
+	) : (
+		<div className='h-screen w-screen bg-black'>
+			<button
+				className='bg-spotify-green'
+				onClick={() => signIn('spotify')}
+			>
+				Log in
+			</button>
 		</div>
 	);
 };
