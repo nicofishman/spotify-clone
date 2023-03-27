4fa256799ea6962c492b0c82a11a4d67e6d1bfee
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-03-27 20:25:30 -0300

    background gradient

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,11 +9,15 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
 	const { data: session } = useSession();
 	return session ? (
-		<div className='grid h-screen w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] grid-areas-layout'>
-			<Sidebar />
-			<TopBar />
-			<main className='bg-blue-500 grid-in-main-view'>{children}</main>
-			<NowPlaying />
+		<div className='h-screen w-full bg-black'>
+			<div className='grid h-screen w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] bg-[linear-gradient(rgba(88,232,128,.5)_0,#121212_100%)] grid-areas-layout'>
+				<Sidebar />
+				<TopBar />
+				<main className='overflow-auto px-8 pt-6 grid-in-main-view'>
+					{children}
+				</main>
+				<NowPlaying />
+			</div>
 		</div>
 	) : (
 		<div className='h-screen w-screen bg-black'>
--- src/components/Layout/TopBar.tsx
+++ src/components/Layout/TopBar.tsx
@@ -11,8 +11,8 @@ export const TopBar = () => {
 	const router = useRouter();
 
 	return (
-		<header className='sticky top-0 h-16 bg-black grid-in-top-bar'>
-			<div className='flex h-full w-full justify-between bg-[#58e880]/50 px-8 py-4'>
+		<header className='sticky top-0 h-16 grid-in-top-bar'>
+			<div className='flex h-full w-full justify-between px-8 py-4'>
 				<div className='flex items-center gap-x-4'>
 					<GoBackButton
 						onClick={() => {
