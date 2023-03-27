9245a994a1cdac0c3521fdbf7a416d6b95f3d4c2
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-03-27 20:25:51 -0300

    icons import

--- src/types/Icons.ts
+++ src/types/Icons.ts
@@ -1,30 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import logo from '@/assets/icons/logo.svg';
-import homeDefault from '@/assets/icons/home.svg';
-import homeActive from '@/assets/icons/home-active.svg';
-import searchDefault from '@/assets/icons/search.svg';
-import searchActive from '@/assets/icons/search-active.svg';
-import collectionDefault from '@/assets/icons/collection.svg';
-import collectionActive from '@/assets/icons/collection-active.svg';
-import plus from '@/assets/icons/plus.svg';
-import heart from '@/assets/icons/heart.svg';
+import arrowDown from '@/assets/icons/arrow-down.svg';
+import chevronLeft from '@/assets/icons/chevron-left.svg';
+import chevronRight from '@/assets/icons/chevron-right.svg';
 import collaborative from '@/assets/icons/collaborative.svg';
-import likeDefault from '@/assets/icons/like.svg';
-import likeActive from '@/assets/icons/like-active.svg';
-import pictureInPicture from '@/assets/icons/picture-in-picture.svg';
-import next from '@/assets/icons/next.svg';
-import previous from '@/assets/icons/previous.svg';
-import play from '@/assets/icons/play.svg';
-import pause from '@/assets/icons/pause.svg';
-import shuffle from '@/assets/icons/shuffle.svg';
-import repeat from '@/assets/icons/repeat.svg';
+import collectionActive from '@/assets/icons/collection-active.svg';
+import collectionDefault from '@/assets/icons/collection.svg';
 import connect from '@/assets/icons/connect.svg';
 import fullscreen from '@/assets/icons/fullscreen.svg';
+import heart from '@/assets/icons/heart.svg';
+import homeActive from '@/assets/icons/home-active.svg';
+import homeDefault from '@/assets/icons/home.svg';
+import likeActive from '@/assets/icons/like-active.svg';
+import likeDefault from '@/assets/icons/like.svg';
+import logo from '@/assets/icons/logo.svg';
 import lyrics from '@/assets/icons/lyrics.svg';
+import next from '@/assets/icons/next.svg';
+import pause from '@/assets/icons/pause.svg';
+import pictureInPicture from '@/assets/icons/picture-in-picture.svg';
+import play from '@/assets/icons/play.svg';
+import plus from '@/assets/icons/plus.svg';
+import previous from '@/assets/icons/previous.svg';
 import queue from '@/assets/icons/queue.svg';
+import repeat from '@/assets/icons/repeat.svg';
+import searchActive from '@/assets/icons/search-active.svg';
+import searchDefault from '@/assets/icons/search.svg';
+import shuffle from '@/assets/icons/shuffle.svg';
 import volume from '@/assets/icons/volume.svg';
 
 export const Icons = {
+	arrowDown: arrowDown,
 	logo: logo,
 	homeActive: homeActive,
 	homeDefault: homeDefault,
@@ -49,4 +53,6 @@ export const Icons = {
 	lyrics: lyrics,
 	queue: queue,
 	volume: volume,
+	chevronRight: chevronRight,
+	chevronLeft: chevronLeft,
 } as const;
