3c14e07243d968690a966fce5444e67d428f669c
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-09-14 15:13:46 -0300

    show and episode page

---
+++ src/pages/episode/[episodeId].tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout/Layout';
+import React from 'react';
+
+const EpisodePage = () => {
+	return (
+		<Layout>
+			<p>EpisodePage</p>
+		</Layout>
+	);
+};
+
+export default EpisodePage;
---
+++ src/pages/show/[showId].tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout/Layout';
+import React from 'react';
+
+const ShowPage = () => {
+	return (
+		<Layout>
+			<p>ShowPage</p>
+		</Layout>
+	);
+};
+
+export default ShowPage;
