498ec457c47b4e1a314d823509cc7e671676c292
nicofishman <74215078+nicofishman@users.noreply.github.com>
2023-09-14 15:10:25 -0300

    project settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,44 +1,42 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-	ignorePatterns: ['node_modules', 'dist', 'coverage', 'tailwind.config.cjs'],
-	overrides: [
-		{
-			extends: [
-				'plugin:@typescript-eslint/recommended-requiring-type-checking',
-			],
-			files: ['*.ts', '*.tsx'],
-			parserOptions: {
-				project: 'tsconfig.json',
-			},
-		},
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-	},
-	plugins: ['@typescript-eslint'],
-	extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
-	rules: {
-		'@typescript-eslint/no-misused-promises': [
-			'error',
-			{
-				checksVoidReturn: false,
-			},
-		],
-		'@typescript-eslint/consistent-type-imports': [
-			'warn',
-			{
-				prefer: 'type-imports',
-				fixStyle: 'inline-type-imports',
-			},
-		],
-		'@typescript-eslint/no-unused-vars': [
-			'warn',
-			{ argsIgnorePattern: '^_' },
-		],
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-	},
+  ignorePatterns: ['node_modules', 'dist', 'coverage', 'tailwind.config.cjs'],
+  overrides: [
+    {
+      extends: [
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    indent: ['error', 2],
+  },
 };
 
 module.exports = config;
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,12 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
-	tabWidth: 4,
-	useTabs: true,
-	jsxSingleQuote: true,
-	singleQuote: true,
-	semi: true,
-	bracketSameLine: false,
-	plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: true,
+  bracketSameLine: false,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
 
 module.exports = config;
